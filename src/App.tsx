d5d221d34e6640ffbd004c96964956d269d6c4e7
dsaints 23444 <ddelossantosmateo@gmail.com>
2020-03-05 16:01:54 -0400

    Adding helper and setting up graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import './App.css';
-import { Gitgraph } from '@gitgraph/react';
+import { Gitgraph, TemplateName } from '@gitgraph/react';
+
+
 
 function App() {
   return (
-    <Gitgraph>
+    <Gitgraph options={{
+      template:  TemplateName.BlackArrow,
+      reverseArrow: false
+    }}>
       {(gitgraph) => {
         const master = gitgraph.branch("master");
-        master.commit("Initial commit")
+        master.commit("Initial commit").tag("v1.0")
+
+
+        const develop = gitgraph.branch("develop")
+        develop.commit("initial commit")
+
+        const features = gitgraph.branch('feature')
+        features.commit('Setting up component')
+
+        develop.merge(features, 'merge feature')
+
+        master.merge(develop, "new release").tag("v1.0.1")
+
+        const releases = gitgraph.branch("release")
+        releases.merge(master, "new release ready").tag("v1.0.1").tag('First release')
+
+
+        
       }}
     </Gitgraph>
   );
